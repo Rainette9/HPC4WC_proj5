87f8e51c02f4cbf3d76a6b519895baa1c12704b7
Alitzel <alitzel_macias@hotmail.com>
2025-09-04 14:25:14 +0200

    Add correct baseline results

--- bash_baseline.sh
+++ bash_baseline.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 #SBATCH --job-name=baseline_2048_2048   # Job name
 #SBATCH --output=error_outs/baseline_2048_2048.out      # Output file (%j = job ID)
-#SBATCH --time=10:00:00            # Max run time (HH:MM:SS)
+#SBATCH --time=05:00:00            # Max run time (HH:MM:SS)
 #SBATCH --partition=normal         # Partition/queue
 #SBATCH --nodes=1
 #SBATCH --hint=nomultithread
@@ -20,8 +20,8 @@ nz_len=64
 
 echo "nx_len = $nx_len, ny_len = $ny_len"
 
-out_file="experiments_final/out_baseline4_${nx_len}_${ny_len}.txt"
-dims_file="experiments_final/dims_baseline4_${nx_len}_${ny_len}.txt"
+out_file="experiments_final/out_baseline_${nx_len}_${ny_len}.txt"
+dims_file="experiments_final/dims_baseline_${nx_len}_${ny_len}.txt"
 
 # if [ ! -f "$out_file" ]; then
 echo "runtimes = [[[0.0]*73 for _ in range(73)]for _ in range(6)]" > "$out_file"
@@ -48,7 +48,7 @@ for iteration in $(seq 1 5); do
           #           echo -n "Cpus_allowed_list: "; awk "/Cpus_allowed_list/ {print \$2}" /proc/self/status;
           #           numactl --show | egrep "cpubind|membind"'
             
-            output=$(srun --ntasks-per-node=$nranks --cpus-per-task=$nthreads --cpu-bind=cores --distribution=block:block --mem-bind=local ./stencil2d-kparallel_mpi.x --nx $nx_len --ny $ny_len --nz $nz_len --num_iter 1024)
+            output=$(srun -N 1 -n $nranks --ntasks-per-node=$nranks --cpus-per-task=$nthreads --cpu-bind=cores --distribution=block:block --mem-bind=local ./stencil2d-kparallel_mpi.x --nx $nx_len --ny $ny_len --nz $nz_len --num_iter 1024)
 
             data_line=$(echo "$output" | grep -oP '\[\s*\d+,\s*\d+,\s*\d+,\s*\d+,\s*\d+,\s*[\d.E+-]+\]')
             runtime=$(echo "$data_line" | sed 's/.*\[//' | sed 's/\]//' | cut -d',' -f6 | xargs)
